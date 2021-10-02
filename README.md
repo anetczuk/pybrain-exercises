395063503e9a2eb8708229244ba43ed64644c000
anetczuk <anetczuk@o2.pl>
2021-10-02 22:47:09 +0200

    Add environment previews

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Exercises with pybrain
+
+Exercises with pybrain.
+
+
+## Available reinforcement learning environments
+
+[![shipsteer](doc/env/shipsteer-small.png "shipsteer")](doc/env/shipsteer-big.png)
+[![acrobot](doc/env/acrobot-small.png "acrobot")](doc/env/acrobot-big.png)
+[![ccrl-glass](doc/env/ccrl-glass-small.png "ccrl-glass")](doc/env/ccrl-glass-big.png)
+[![flexcube](doc/env/flexcube-small.png "flexcube")](doc/env/flexcube-big.png)
+[![johnnie](doc/env/johnnie-small.png "johnnie")](doc/env/johnnie-big.png)
--- doc/generate_small.sh
+++ doc/generate_small.sh
@@ -2,19 +2,23 @@
 
 set -eu
 
+## ‘**’ used in a filename expansion context will match all files and zero or more directories and subdirectories
+shopt -s globstar
+
+
 ## works both under bash and sh
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 
-big_suffix="-big."
-small_suffix="-small."
+big_suffix="-big.png"
+small_suffix="-small.png"
 
-for filename in $SCRIPT_DIR/*.png; do
-    if [[ $filename != *"$big_suffix"* ]]; then
+for filename in $SCRIPT_DIR/**; do
+    if [[ $filename != *"${big_suffix}"* ]]; then
         continue
     fi
     small_name=${filename/$big_suffix/$small_suffix}
     echo "converting: $filename -> $small_name"
-    convert $filename -resize 400 $small_name
+    convert $filename -resize 240 $small_name
     #convert $filename -resize 200x100 $small_name
 done
